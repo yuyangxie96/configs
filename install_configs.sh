8ea1fc2cee6c48862db46f14e7d38ab3d709844f
Yuyang Xie <yuyangxie23@gmail.com>
2024-02-01 14:31:03 +0000

    configs fix

--- install_configs.sh
+++ install_configs.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+cp ./.p10k.zsh ~
 cp ./.inputrc ~
 cp ./.tmux.conf ~
 cp ./.vimrc ~
 cp ./.zshrc ~
+cp ./.zshenv ~
+cp ./.zsh.antigen ~
 cp ./.environment.zsh ~
+cp ./ccls-config ~
+cp ./my_aliases.zsh ~
 cp ./compile_flags.txt ~
-
